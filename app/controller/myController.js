7c8cd93939bf7c496104b237baf64a2f483e3ac0
paras <rockypro92@gmail.com>
2016-03-03 00:13:24 +0530

    Add a new item to the list

--- app/controller/myController.js
+++ app/controller/myController.js
@@ -1,9 +1,14 @@
-myApp.controller('myController', function($scope, $http, classifiedsFactory, $mdSidenav){
+myApp.controller('myController', function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast){
     
   classifiedsFactory.getClassifieds().then(function(items){
     $scope.items = items.data;
     // console.log($scope.items);
   });
+
+  var contact = {
+    name: "Flash",
+    phone: "555-555-444"
+  }
 
   $scope.openSidebar = function() {
     $mdSidenav('left').open();
@@ -13,5 +18,19 @@ myApp.controller('myController', function($scope, $http, classifiedsFactory, $md
     $mdSidenav('left').close();
   }
 
+  $scope.saveItem = function(item) {
+    if(item) {
+      item.contact = contact;
+      $scope.items.push(item);
+      $scope.item = {};
+      $scope.closeSidebar();
+      $mdToast.show(
+        $mdToast.simple()
+          .content("Item Saved!")
+          .position('top, right')
+          .hideDelay(3000)
+      );    
+    }
+  }
 
 }); 
